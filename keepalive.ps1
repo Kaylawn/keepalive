f95ac8de9921549856587a01761998bcce956e07
Kaylon Jackson <118871388+Kaylawn@users.noreply.github.com>
2025-04-04 10:14:09 -0500

    Add files via upload

---
+++ keepalive.ps1
@@ -0,0 +1,12 @@
+Write-Host "Keep alive w/ scroll lock..."
+
+$WShell = New-Object -com "Wscript.Shell"
+
+while ($true)
+{
+$WShell.sendkeys("{SCROLLLOCK}")
+Start-Sleep -Milliseconds 100
+
+$WShell.sendkeys("{SCROLLLOCK}")
+Start-Sleep -Seconds 240
+}
